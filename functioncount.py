fdb348a75db2b1f28e3a7472c6c43ccda02f42a5
Nuverin <90313515+nuverin@users.noreply.github.com>
2021-10-06 17:33:38 +0700

    Add files via upload
    three day with python 
    #LEARN PYTHON
    #About Function

---
+++ functioncount.py
@@ -0,0 +1,9 @@
+#define function 
+#check what it true or false
+#define count wit function
+x = "Name on your hear on your"
+# you can 
+s, s1, s2, s3 = "o", "a", "y", "n"
+
+print("Count", x.count(s1) ), print("False Or true > " , x.endswith(s1)) , print(x.casefold())
+
