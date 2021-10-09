9404c0622c14962da4369cab9f983ba37febdf54
Nuverin <90313515+nuverin@users.noreply.github.com>
2021-10-09 22:13:44 +0700

    Add files via upload
    Five Day in python
    #MemPelajari Tentang Function String,list,Dictionary
    #Learn PYTHON

---
+++ funct2.py
@@ -0,0 +1,8 @@
+#fungsi menambah string dilist
+books = []
+Text_input = input("Booku ;  ")
+#SeBelum ditamba
+for x  in Text_input:
+    books.append(x != 0>1)
+    print("Buku sudah ditambahkan ; ", books[0])
+    break
