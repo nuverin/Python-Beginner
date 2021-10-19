b2a73b64a1f0d860e6e77de7332068e04000fef7
Nuverin <90313515+nuverin@users.noreply.github.com>
2021-10-19 21:46:10 +0700

    Add files via upload
    Hash adalah pencarian nilai kunci yang artinya hash mencari nilai memori yang terdapat pada nilai array,
    Fungsi Hash Juga Membuat string menjadi semacam Bilangan bulat
    #learn python 
    #Hash Table

---
+++ hash.py
@@ -0,0 +1,21 @@
+import random
+def id():
+    random.Random(9)
+    return random.random()
+
+
+def Name(name):
+    return name
+
+    
+def gets():
+    return prince
+
+prince = {
+    'Hilmy' : id(),
+    'hilmy' : id()
+}
+print("Id dari Nama -",prince['Hilmy'])
+print("Id dari Nama -",prince['hilmy'])
+
+    
